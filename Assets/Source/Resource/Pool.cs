b5800c0e83b614ce1db43456d825cb1ae93ab766
yuchangjun <yw-jun888@163.com>
2017-08-26 15:28:26 +0800

    Pool Description

---
+++ Assets/Source/Resource/Pool.cs
@@ -0,0 +1,15 @@
+﻿
+using Game;
+/// <summary>
+/// Attach 挂在GameObject 上 ，当GameObject销毁时，负责减少引用计数
+/// Pool 对 GameObject层面进行缓存，同时控制 GameObject 的显示、隐藏 及销毁
+/// Resource 对 资源进行加载
+/// Cache 对 AssetBundle层面进行缓存
+/// </summary>
+class Pool
+{
+    public void Load(AssetInfo assetInfo)
+    {
+
+    }
+}
